bb152f09a4e661f8f2590baa77173b1240a1dd5f
Molly Benjaminson <benjaminsonmolly@gmail.com>
2019-03-14 15:33:13 -0700

    Basic functionality and simple styling added.

--- background.js
+++ background.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
   var checkPageButton = document.getElementById('checkPage');
+  var inspirationArray = [
+    'Get a coffee.', 'Try the same step twice.', 'Use a PNG.', 'Take a screenrecording of your process.'
+  ];
   if(checkPageButton){
     checkPageButton.addEventListener('click', function() {
-      alert("You've been inspired.");
+      var random = inspirationArray[Math.floor(Math.random()*inspirationArray.length)];
+      alert(random);
     }, false);
   }
 }, false);

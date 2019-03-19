594fe681b702839dd1cbb38560b3862fa6dcfe3b
Molly Benjaminson <benjaminsonmolly@gmail.com>
2019-03-19 10:03:07 -0700

    Added some more random results.

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   var inspireButton = document.getElementById('inspire');
   var inspirationArray = [
-    'Go get a coffee.', 'Do the same step twice.', 'Use a different file format.', 'Screenrecord your process.'
+    'Go get a coffee.', 'Do the same step twice.', 'Use a different file format.', 'Screenrecord your process.', 'Resize your browser window.', 'Invert all the colors.', 'Click every button three times in a row.'
   ];
   if(inspireButton){
     inspireButton.addEventListener('click', function() {
